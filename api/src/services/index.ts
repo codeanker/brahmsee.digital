914d1fb445c0d5b0fe6e88e4409fef8bfe40393e
danielswiatek <daniel.swiatek@codeanker.de>
2023-06-07 20:18:23 +0200

    add gliederung service

--- api/src/services/index.ts
+++ api/src/services/index.ts
@@ -1,8 +1,10 @@
 import { router } from '../trpc'
 import { authenticationRouter } from './authentication'
+import { gliederungRouter } from './gliederung'
 import { userRouter } from './user'
 
 export const appRouter = router({
   user: userRouter,
   authenication: authenticationRouter,
+  gliederung: gliederungRouter,
 })
