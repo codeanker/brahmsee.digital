5023979392bc9bef65c78dae1e1491ae73593b94
Axel Rindle <mail@axelrindle.de>
2023-07-17 23:26:51 +0200

    fix: add missing route

--- frontend/src/views/Users/_routes.js
+++ frontend/src/views/Users/_routes.js
@@ -5,6 +5,11 @@ export default [
     component: () => import('./UsersList.vue'),
   },
   {
+    name: 'UserCreate',
+    path: '/users/create',
+    component: () => import('./UserCreate.vue'),
+  },
+  {
     name: 'UserDetail',
     path: '/users/:userId/detail',
     component: () => import('./UserDetail.vue'),
