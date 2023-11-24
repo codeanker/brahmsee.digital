a98903d2c7beaf6f073ea79454445c96923a5ef7
Lasse Eggers <lasse.eggers@codeanker.de>
2023-11-24 21:35:55 +0100

    für axel

--- frontend/src/views/Dashboard/DashboardView.vue
+++ frontend/src/views/Dashboard/DashboardView.vue
@@ -1,15 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <div class="mt-6 flex flex-col items-center gap-16">
-      <img
-        src="@/assets/images/logo.svg"
-        class="w-64"
-      />
-      <h1 class="text-2xl font-semibold">
-        Hier kommt die Brahmsee Planung für <span class="text-primary-600 underline">2024</span>
-      </h1>
-    </div>
+  <div class="container">
+    <h1>Dashboard / Home Übersicht... ```veranstaltung/:veranstalungId/dashboard```</h1>
   </div>
 </template>
