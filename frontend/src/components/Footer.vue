b38add095a5af034c2d5e32d82a60d2394c6e189
danielswiatek <daniel.swiatek@codeanker.de>
2023-11-24 22:00:54 +0100

    edit footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,21 +1,19 @@
 <script setup lang="ts">
-const year = new Date().getFullYear()
+let version = '0.0.0'
+let commitHash = '0000000'
 </script>
 
 <template>
-  <footer class="bg-gray-100 text-sm">
-    <div class="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-      <div class="sm:flex sm:items-center">
-        <p class="mt-4 text-center text-sm text-gray-500 dark:text-gray-400 lg:mt-0 lg:text-right">
-          Copyright &copy; {{ year }}. All rights reserved.
-        </p>
-        <div class="flex justify-center text-teal-600 sm:justify-start">
-          <img
-            src="/codeanker.png"
-            class="h-4"
-          />
-        </div>
-      </div>
+  <footer class="text-sm text-center">
+    <div class="w-full text-center mb-3 text-sm leading-snug opacity-50">
+      Made with <span class="text-primary-700">♥︎</span> in Germany · Supported by
+      <a
+        class="underline"
+        href="https://codeanker.de/"
+        rel="noreferrer"
+        >CODEANKER</a
+      >
+      (v{{ version }}#{{ commitHash }})
     </div>
   </footer>
 </template>
