464422776deb501f9cd463f96e789116ec92c185
Torge Jürgensen <torge.juergensen@codeanker.de>
2023-11-25 00:04:16 +0100

    Refactor user references to person

--- frontend/src/helpers/personProfileImage.ts
+++ frontend/src/helpers/personProfileImage.ts
@@ -1,9 +1,9 @@
 import MD5 from 'crypto-js/md5'
 
-export default function (user?: any, size = 50, default_image = 'mp', allowed_rating = 'g', force_default = 'n') {
+export default function (person?: any, size = 50, default_image = 'mp', allowed_rating = 'g', force_default = 'n') {
   return (
     'https://secure.gravatar.com/avatar/' +
-    MD5(user?.email?.toLowerCase()?.trim()) +
+    MD5(person?.email?.toLowerCase()?.trim()) +
     '?size=' +
     size +
     '&default=' +
