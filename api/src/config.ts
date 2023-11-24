843758af5ad2f736e1ac7c9103558b8927034643
Barne Jürgensen <barne.juergensen@codeanker.de>
2023-11-24 11:59:46 +0100

    fix types

--- api/src/config.ts
+++ api/src/config.ts
@@ -1,11 +1,11 @@
 import path from 'node:path'
-import { fileURLToPath } from 'node:url'
+// import { fileURLToPath } from 'url'
 
 import config from 'config'
 import { z } from 'zod'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = path.dirname(__filename)
 
 const baseConfig = config.util.loadFileConfigs(path.join(__dirname, '..', 'config'))
 
