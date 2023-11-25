c979b1a9a807882561eb95638d7f7c42e51861a9
Torge Jürgensen <torge.juergensen@codeanker.de>
2023-11-25 20:20:31 +0100

    Add new values to QualificationErsteHilfe enum

---
+++ api/src/enumMappings/NahrungsmittelIntoleranz.ts
@@ -0,0 +1,10 @@
+import { NahrungsmittelIntoleranz } from '@prisma/client'
+
+import { defineEnumMapping } from './defineEnumMapping'
+
+export const NahrungsmittelIntoleranzMapping = defineEnumMapping<NahrungsmittelIntoleranz>({
+  FRUCTOSE: { human: 'Fructose' },
+  LAKTOSE: { human: 'Laktose' },
+  GLUTEN: { human: 'Gluten' },
+  SCHWEIN: { human: 'Schweinefleisch' },
+})
