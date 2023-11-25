fb0acb171b474f989ccf567fdb1664ae43a31529
Momme Jürgensen <momme@juergensen.me>
2023-11-25 11:31:17 +0100

    add some tests

--- frontend/tests/verwaltung-gliederungen.e2e.ts
+++ frontend/tests/verwaltung-gliederungen.e2e.ts
@@ -2,7 +2,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import dayjs from 'dayjs'
 import { Browser, chromium, BrowserContext, firefox, webkit, Page } from 'playwright'
-import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it, vi } from 'vitest'
+
+import { insertJwtToken } from './helpers/insertJwtToken'
 
 import { testUtils } from '@codeanker/api'
 
@@ -11,16 +13,26 @@ describe(`View: verwaltung/gliederungen`, () => {
   let context: BrowserContext
   let name = ''
   let data: Awaited<ReturnType<typeof testUtils.createMock>>
+  const runId = (Math.random() + 1).toString(36).substring(7)
+
   beforeAll(async () => {
     name = `test-${chromium.name()}-${dayjs().format('YYYY-MM-DD-HH-mm-ss')}`
     browser = await chromium.launch({ headless: true })
     context = await browser.newContext({ ignoreHTTPSErrors: true })
-    await testUtils.cleanup()
-    data = await testUtils.createMock()
+    data = await testUtils.createMock(runId)
   })
   afterAll(async () => {
     browser?.close()
-    await testUtils.cleanup()
+    await testUtils.cleanup(runId)
   })
-  it.skip('Write tests.', async () => {})
+  it('Gliederung auflisten', async () => {
+    const page = await context.newPage()
+    await insertJwtToken(page, data.accessToken)
+
+    await page.goto(`https://localhost.codeanker.com:8080/verwaltung/gliederungen`)
+    await vi.waitUntil(async () => (await page.getByText('DLRG Bundesverband').isVisible()) === true)
+    await page.screenshot({ path: `${__dirname}/screenshots/${name}_verwaltung-gliederung.png` })
+  })
+  it.skip('Gliederung erstellen', async () => {})
+  it.skip('Gliederung details ansehen', async () => {})
 })
