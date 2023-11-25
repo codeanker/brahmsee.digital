4ba01e6f1bd004fb6520a57e1335d45afaf098f0
Torge Jürgensen <torge.juergensen@codeanker.de>
2023-11-25 22:17:50 +0100

    Fix import statement in trpc.ts

--- api/src/trpc.ts
+++ api/src/trpc.ts
@@ -6,7 +6,7 @@ import { type Context } from './context'
 import { logger } from './logger'
 import { trpc_call_duration } from './metrics'
 import prisma from './prisma'
-// import { isDevelopment } from './util/is-production'
+import { isDevelopment } from './util/is-production'
 
 const t = initTRPC.context<Context>().create({
   transformer: superjson,
@@ -26,12 +26,11 @@ const loggerMiddleware = middleware(async (opts) => {
     durationMs
   )
 
-  // const stack = isDevelopment() ? result.error.stack : result.error.message
-
   if (result.ok) logger.info(`[${opts.ctx.accountId ?? 'public'}] ${meta.path}.${meta.type} [${durationMs}ms]`)
   else {
+    const stack = isDevelopment() ? result.error.stack : result.error.message
     // maybe dont log all errors
-    logger.error(`[${opts.ctx.accountId ?? 'public'}] ${meta.path}.${meta.type} [${durationMs}ms]`)
+    logger.error(`[${opts.ctx.accountId ?? 'public'}] ${meta.path}.${meta.type} [${durationMs}ms] ${stack}`)
   }
   return result
 })
