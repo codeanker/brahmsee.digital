b500d58a61e7c887a7e39c609889d7c8784d11d0
Lasse Eggers <lasse.eggers@codeanker.de>
2023-11-25 16:35:26 +0100

    fix layout

--- frontend/src/components/UIComponents/Button.vue
+++ frontend/src/components/UIComponents/Button.vue
@@ -14,7 +14,7 @@ withDefaults(
 type Color = 'primary' | 'secondary' | 'success' | 'warning' | 'danger'
 const colors: Record<Color, string> = {
   primary: 'text-white bg-primary-500 hover:bg-primary-600 focus:outline-primary-500',
-  secondary: 'text-white bg-primary-300 hover:bg-primary-400 focus:outline-primary-500',
+  secondary: 'bg-primary-200 hover:bg-primary-300 focus:outline-primary-500 text-primary-600',
   success: '',
   warning: 'text-white bg-orange-500 hover:bg-orange-400 focus:outline-orange-500',
   danger: 'text-white bg-red-500 hover:bg-red-400 focus:outline-red-500',
@@ -30,6 +30,7 @@ const colors: Record<Color, string> = {
         'block rounded-lg px-3 py-2 text-center text-sm font-medium',
         'focus:outline focus:outline-2 focus:outline-offset-2',
         'disabled:cursor-not-allowed',
+        'w-full inline-block whitespace-nowrap',
         colors[color],
       ].join(' ')
     "
