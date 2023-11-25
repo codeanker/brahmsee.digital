2cffb3bf250bb52a3bc26cdce72c8d1602927c58
danielswiatek <daniel.swiatek@codeanker.de>
2023-11-25 22:17:13 +0100

    Add Dashboard route to router

---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="dashboard">
+    <h3>Welcome to the Dashboard!</h3>
+    <!-- Add your dashboard content here -->
+  </div>
+</template>
