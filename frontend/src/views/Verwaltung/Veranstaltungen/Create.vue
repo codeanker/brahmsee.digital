5c7ea98ec33717d8dd41a34bc8d96481b651c7a9
danielswiatek <daniel.swiatek@codeanker.de>
2023-11-25 10:41:31 +0100

    add veranstaltung create

--- frontend/src/views/Verwaltung/Veranstaltungen/Create.vue
+++ frontend/src/views/Verwaltung/Veranstaltungen/Create.vue
@@ -1,7 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BasicCheckbox from '@/components/BasicCheckbox.vue'
+</script>
 
 <template>
   <div>
-    <h5>Veranstaltungen Create ```verwaltung/veranstaltungen/erstellen```</h5>
+    <h5>Veranstaltung anlegen</h5>
+    <BasicCheckbox
+      v-model="checked"
+      :label="label"
+      :options="options"
+      :value="value"
+    ></BasicCheckbox>
   </div>
 </template>
