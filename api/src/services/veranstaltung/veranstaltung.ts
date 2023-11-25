2172f5acf58e8e67d9da67e506793dc2997ca359
Torge Jürgensen <torge.juergensen@codeanker.de>
2023-11-25 13:02:12 +0100

    Add verwaltungPatch API method

--- api/src/services/veranstaltung/veranstaltung.ts
+++ api/src/services/veranstaltung/veranstaltung.ts
@@ -4,10 +4,14 @@ import {
   ZVeranstaltungVerwaltungCreateInputSchema,
   veranstaltungVerwaltungCreate,
 } from './veranstaltungVerwaltungCreate'
+import { ZVeranstaltungVerwaltungPatchInputSchema, veranstaltungVerwaltungPatch } from './veranstaltungVerwaltungPatch'
 
 export const veranstaltungRouter = router({
   verwaltungCreate: protectedProcedure(['ADMIN'])
     .input(ZVeranstaltungVerwaltungCreateInputSchema)
     .mutation(veranstaltungVerwaltungCreate),
+  verwaltungPatch: protectedProcedure(['ADMIN'])
+    .input(ZVeranstaltungVerwaltungPatchInputSchema)
+    .mutation(veranstaltungVerwaltungPatch),
   // TODO: add your API methods here
 })
---
+++ api/src/services/veranstaltung/veranstaltungVerwaltungPatch.ts
@@ -0,0 +1,36 @@
+import z from 'zod'
+
+import prisma from '../../prisma'
+import type { AuthenticatedContext } from '../../trpc'
+
+export const ZVeranstaltungVerwaltungPatchInputSchema = z.strictObject({
+  data: z.strictObject({
+    id: z.number(),
+    name: z.string().optional(),
+    beginn: z.date().optional(),
+    ende: z.date().optional(),
+    ort: z.string().optional(),
+    meldebeginn: z.date().optional(),
+    meldeschluss: z.date().optional(),
+    maxTeilnehmende: z.number().int().optional(),
+    teilnahmegebuehr: z.number().int().optional(),
+  }),
+})
+
+export type TVeranstaltungVerwaltungPatchInputSchema = z.infer<typeof ZVeranstaltungVerwaltungPatchInputSchema>
+
+type VeranstaltungVerwaltungPatchOptions = AuthenticatedContext & {
+  input: TVeranstaltungVerwaltungPatchInputSchema
+}
+
+export async function veranstaltungVerwaltungPatch(options: VeranstaltungVerwaltungPatchOptions) {
+  return prisma.veranstaltung.update({
+    where: {
+      id: options.input.data.id,
+    },
+    data: options.input.data,
+    select: {
+      id: true,
+    },
+  })
+}
