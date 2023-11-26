9adc1e9c85f0a705629e45049032260cd581be7a
danielswiatek <daniel.swiatek@codeanker.de>
2023-11-26 11:55:10 +0100

    fix routes

--- frontend/src/views/Development/routes.ts
+++ frontend/src/views/Development/routes.ts
@@ -4,11 +4,25 @@ const routesDevelopment: Route[] = [
   {
     name: 'Development',
     path: '/development',
+    meta: {
+      breadcrumbs: [
+        {
+          text: 'Entwicklung',
+        },
+      ],
+    },
     children: [
       {
         name: 'Komponenten',
         path: 'components',
         component: () => import('./Components.vue'),
+        meta: {
+          breadcrumbs: [
+            {
+              text: 'Komponenten',
+            },
+          ],
+        },
       },
     ],
   },
