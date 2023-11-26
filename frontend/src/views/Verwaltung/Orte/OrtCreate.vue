a2adc3a18f51b9a8aa15d5e36dc2c1c0f189f61a
Torge Jürgensen <torge.juergensen@codeanker.de>
2023-11-26 12:16:39 +0100

    Add ortRouter to serviceRouter

--- frontend/src/views/Verwaltung/Orte/OrtCreate.vue
+++ frontend/src/views/Verwaltung/Orte/OrtCreate.vue
@@ -1,7 +1,39 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import { apiClient } from '@/api'
+import BasicInput from '@/components/BasicInputs/BasicInput.vue'
+import Button from '@/components/UIComponents/Button.vue'
+import type { RouterInput } from '@codeanker/api'
+import { ValidateForm } from '@codeanker/validation'
+
+let model = ref<Partial<RouterInput['ort']['verwaltungCreate']['data']>>({
+  name: '',
+})
+
+async function create() {
+  await apiClient.ort.verwaltungCreate.mutate({
+    data: model.value as RouterInput['ort']['verwaltungCreate']['data'],
+  })
+}
+</script>
 
 <template>
-  <div>
-    <h5>Orte Create ```verwaltung/orte/erstellen```</h5>
-  </div>
+  <ValidateForm
+    class="space-y-4"
+    @submit="create"
+  >
+    <h5>Ort anlegen</h5>
+    <BasicInput
+      v-model="model.name"
+      required
+      label="Name"
+      placeholder="Name des Ortes"
+    />
+    <Button
+      type="submit"
+      color="primary"
+      >Anlegen</Button
+    >
+  </ValidateForm>
 </template>
