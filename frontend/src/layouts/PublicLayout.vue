2a830eb03ea1b5ec46e067f9f99c374d6706c16d
Torge Jürgnsen <torge.juergensen@codeanker.de>
2023-11-28 20:54:56 +0100

    restore old layout

--- frontend/src/layouts/PublicLayout.vue
+++ frontend/src/layouts/PublicLayout.vue
@@ -7,12 +7,12 @@ const route = useRoute()
 </script>
 
 <template>
-  <div class="relative flex flex-col lg:grid lg:grid-cols-2 bg-white">
+  <div class="flex flex-col lg:grid lg:grid-cols-2 bg-white">
     <PublicHeaderBg
-      class="lg:block relative h-72 lg:h-screen lg:sticky lg:top-0 shrink-0"
+      class="lg:block relative h-72 lg:h-screen lg:sticky lg:top-0 shrink-0 lg:order-2"
       :class="{ hidden: route.meta?.hidePublicHeaderBgOnMobile }"
     />
-    <div class="relative p-6 lg:h-full">
+    <div class="p-6 lg:h-full lg:order-1">
       <router-view />
     </div>
   </div>
