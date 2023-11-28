113975dbaaa5ac0de5d37c00f63cbaab93089b48
Torge Jürgnsen <torge.juergensen@codeanker.de>
2023-11-28 20:48:45 +0100

    Fix duplicated logins Inputs

--- frontend/src/layouts/PublicLayout.vue
+++ frontend/src/layouts/PublicLayout.vue
@@ -8,14 +8,11 @@ const route = useRoute()
 
 <template>
   <div class="relative flex flex-col lg:grid lg:grid-cols-2 bg-white">
-    <div class="relative hidden lg:block h-full">
-      <router-view />
-    </div>
     <PublicHeaderBg
       class="lg:block relative h-72 lg:h-screen lg:sticky lg:top-0 shrink-0"
       :class="{ hidden: route.meta?.hidePublicHeaderBgOnMobile }"
     />
-    <div class="relative block lg:hidden p-6">
+    <div class="relative p-6 lg:h-full">
       <router-view />
     </div>
   </div>
