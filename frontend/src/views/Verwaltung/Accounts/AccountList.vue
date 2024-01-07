79dfc437178887b93316c8cdb6dd9aadb2b2934c
danielswiatek <daniel.swiatek@codeanker.de>
2024-01-07 16:52:28 +0100

    Update account list view in Verwaltung module

--- frontend/src/views/Verwaltung/Accounts/AccountList.vue
+++ frontend/src/views/Verwaltung/Accounts/AccountList.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import { ArrowTopRightOnSquareIcon } from '@heroicons/vue/24/outline'
+import { CheckIcon } from '@heroicons/vue/24/outline'
 import { useAsyncState } from '@vueuse/core'
 
 import { apiClient } from '@/api'
-import Button from '@/components/UIComponents/Button.vue'
 import router from '@/router'
 import { roleMapping } from '@codeanker/api/src/enumMappings'
 import { formatDate } from '@codeanker/helpers'
@@ -36,6 +35,12 @@ fetchAccounts()
             scope="col"
             class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900"
           >
+            Name
+          </th>
+          <th
+            scope="col"
+            class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900"
+          >
             E-Mail Adresse
           </th>
           <th
@@ -48,41 +53,34 @@ fetchAccounts()
             scope="col"
             class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
           >
-            Aktivierungsdatum
-          </th>
-          <th
-            scope="col"
-            class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-          >
-            Person
+            Aktiviert
           </th>
         </tr>
       </thead>
       <tbody class="divide-y divide-gray-200 bg-white">
         <tr
-          v-for="person in accountList"
-          :key="person.id"
+          v-for="account in accountList"
+          :key="account.id"
           class="cursor-pointer even:bg-gray-50 hover:bg-gray-100"
-          :title="person.email + ' bearbeiten'"
-          @click="router.push({ name: 'Verwaltung Accountdetails', params: { accountId: person.id } })"
+          :title="account.email + ' bearbeiten'"
+          @click="router.push({ name: 'Verwaltung Accountdetails', params: { accountId: account.id } })"
         >
           <td class="whitespace-nowrap py-5 pl-4 pr-3 text-sm">
-            <div class="font-medium text-gray-900">{{ person.email }}</div>
+            <span>{{ account.person.firstname }} {{ account.person.lastname }}</span>
+          </td>
+          <td class="whitespace-nowrap py-5 pl-4 pr-3 text-sm">
+            <div class="text-gray-900">{{ account.email }}</div>
           </td>
           <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-            {{ roleMapping[person.role].human }}
+            {{ roleMapping[account.role].human }}
           </td>
           <td class="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-            {{ formatDate(person.activatedAt) }}
-          </td>
-          <td
-            class="whitespace-nowrap px-3 py-5 text-sm text-gray-500"
-            @click="router.push({ name: 'Verwaltung Persondetails', params: { personId: person.personId } })"
-          >
-            <Button color="secondary">
-              <ArrowTopRightOnSquareIcon class="h-5" />
-              <span>{{ person.person.firstname }} {{ person.person.lastname }}</span>
-            </Button>
+            <div
+              v-if="account.activatedAt"
+              class="flex items-center"
+            >
+              <CheckIcon class="text-primary-600 h-5 w-5" />{{ formatDate(account.activatedAt) }}
+            </div>
           </td>
         </tr>
       </tbody>
