807bc415f0cf9325fe06db7e5668b06093ce1f10
danielswiatek <daniel.swiatek@codeanker.de>
2024-01-18 20:48:10 +0100

    Refactor Badge component to use dynamic colors

--- frontend/src/components/UIComponents/Badge.vue
+++ frontend/src/components/UIComponents/Badge.vue
@@ -1,7 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    color?: Color
+  }>(),
+  {
+    color: 'danger',
+  }
+)
+type Color = 'muted' | 'primary' | 'success' | 'warning' | 'danger'
+const colors: Record<Color, string> = {
+  muted: 'bg-gray-50 text-gray-600 ring-gray-500/10',
+  primary: 'bg-primary-50 text-primary-600 ring-primary-500/10',
+  success: 'bg-primary-50 text-primary-600 ring-primary-500/10',
+  warning: 'bg-yellow-50 text-yellow-600 ring-yellow-500/10',
+  danger: 'bg-red-50 text-red-600 ring-red-500/10',
+}
+</script>
 
 <template>
-  <span class="whitespace-nowrap rounded-full bg-orange-100 px-2.5 py-0.5 text-sm font-semibold text-orange-700">
+  <span
+    class="whitespace-nowrap inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset"
+    :class="[colors[color]]"
+  >
     <slot></slot>
   </span>
 </template>
