e2b2b34929ad721776a9e660263a9ddc21b0cab1
danielswiatek <daniel.swiatek@codeanker.de>
2024-02-09 15:03:30 +0100

    Update dependencies and seeders

--- api/prisma/seeders/anmeldungen.ts
+++ api/prisma/seeders/anmeldungen.ts
@@ -1,45 +1,48 @@
-import { Essgewohnheit, Gender, PrismaClient } from '@prisma/client'
+import { fakerDE as faker } from '@faker-js/faker'
+import { AnmeldungStatus, Essgewohnheit, Gender, PrismaClient } from '@prisma/client'
 
 import { Seeder } from '.'
 
+faker.seed(123)
+
 const createAnmeldung: Seeder = async function (prisma: PrismaClient) {
   const unterveranstaltung = await prisma.unterveranstaltung.findFirstOrThrow()
+  const entryCount = 100
 
-  await prisma.anmeldung.create({
-    data: {
-      unterveranstaltung: {
-        connect: {
-          id: unterveranstaltung.id,
-        },
+  for (let i = 0; i < entryCount; i++) {
+    const address = await prisma.address.create({
+      data: {
+        city: faker.location.city(),
+        zip: faker.location.zipCode(),
+        street: faker.location.street(),
+        number: faker.location.buildingNumber(),
       },
-      status: 'OFFEN',
-      person: {
-        create: {
-          firstname: 'Steffen',
-          lastname: 'Super',
-          email: 'steffen.super@example.org',
-          telefon: '+49 123 4567890',
-          gender: Gender.MALE,
-          birthday: new Date(),
-          essgewohnheit: Essgewohnheit.OMNIVOR,
-          konfektionsgroesse: 'XL',
-          address: {
-            create: {
-              city: 'Flensburg',
-              zip: '24941',
-              street: 'Holm',
-              number: '16',
-            },
-          },
-          gliederung: {
-            connect: {
-              edv: '1205013',
-            },
-          },
-        },
+    })
+
+    const person = await prisma.person.create({
+      data: {
+        firstname: faker.person.firstName(),
+        lastname: faker.person.firstName(),
+        birthday: faker.date.birthdate({ min: 12, max: 30, mode: 'age' }),
+        gender: faker.helpers.enumValue(Gender),
+        email: faker.internet.email(),
+        telefon: faker.string.numeric('+49151########'),
+        essgewohnheit: faker.helpers.enumValue(Essgewohnheit),
+        konfektionsgroesse: faker.helpers.arrayElement(['S', 'M', 'L', 'XL', 'XXL']),
+        addressId: address.id,
+        gliederungId: unterveranstaltung.gliederungId,
       },
-    },
-  })
+    })
+
+    await prisma.anmeldung.create({
+      data: {
+        unterveranstaltungId: unterveranstaltung.id,
+        status: faker.helpers.enumValue(AnmeldungStatus),
+        tshirtBestellt: faker.datatype.boolean(),
+        personId: person.id,
+      },
+    })
+  }
 }
 
 export default createAnmeldung
