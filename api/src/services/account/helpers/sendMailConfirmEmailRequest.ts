58bf59ba74b69353a31dab26531077167c5c8f55
Torge Jürgensen <torge.juergensen@codeanker.de>
2024-02-09 11:11:10 +0100

    fix importing server resources in client

---
+++ api/src/services/account/helpers/sendMailConfirmEmailRequest.ts
@@ -0,0 +1,12 @@
+import config from '../../../config'
+import { sendMail } from '../../../util/mail'
+
+export async function sendMailConfirmEmailRequest(data: { email: string; activationToken: string }) {
+  const activationUrl = `${config.clientUrl}/registrierung/confirm/${data.activationToken}`
+  sendMail({
+    to: data.email,
+    subject: 'brahmsee.digital Bestätige deine E-Mail Adresse',
+    categories: ['account', 'confirm'],
+    html: `Bitte bestätige deine E-Mail Adresse, indem du auf folgenden Link klickst: <a href="${activationUrl}">${activationUrl}</a>`,
+  })
+}
