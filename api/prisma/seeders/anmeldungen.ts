cc5d494b86afe616029008c7ff7119d09d9c1e32
danielswiatek <daniel.swiatek@codeanker.de>
2024-02-18 18:06:06 +0100

    Add comment field to Anmeldung model

--- api/prisma/seeders/anmeldungen.ts
+++ api/prisma/seeders/anmeldungen.ts
@@ -69,6 +69,7 @@ async function create(prisma: PrismaClient, unterveranstaltung: Unterveranstaltu
       status: faker.helpers.enumValue(AnmeldungStatus),
       tshirtBestellt: faker.datatype.boolean(),
       personId: person.id,
+      comment: faker.lorem.sentence(),
     },
     select: {
       id: true,
