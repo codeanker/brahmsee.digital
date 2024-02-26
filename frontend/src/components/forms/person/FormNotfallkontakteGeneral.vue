603623f13fab16bd9dc508be7c8af5c64c59c788
danielswiatek <daniel.swiatek@codeanker.de>
2024-02-26 09:46:36 +0100

    Add default person template if personen array is empty

--- frontend/src/components/forms/person/FormNotfallkontakteGeneral.vue
+++ frontend/src/components/forms/person/FormNotfallkontakteGeneral.vue
@@ -22,7 +22,11 @@ const emit = defineEmits<{
 
 const model = computed({
   get() {
-    return props.modelValue
+    let model = props.modelValue
+    if (props.modelValue.personen.length === 0) {
+      model.personen.push(personTemplate.value)
+    }
+    return model
   },
   set(val) {
     emit('update:modelValue', val)
@@ -39,14 +43,13 @@ const personTemplate = ref<TKontaktSchema>({
 
 <template>
   <p class="font-medium mb-5">Notfallkontakte</p>
-
   <div
     v-for="(_notfallkontaktPerson, index) in model.personen"
     :key="index"
   >
     <KontaktItem
       v-model="model.personen[index]"
-      show-remove-option
+      :show-remove-option="model.personen.length > 1"
       @remove="model.personen.splice(index, 1)"
     />
     <hr
