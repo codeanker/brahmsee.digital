eacd3ebb8b3ac583a936c44ef2ba6851995bf7d0
Axel Rindle <axel.rindle@codeanker.de>
2024-09-06 19:11:58 +0200

    merge migrations

--- api/prisma/migrations/20231126092320_veranstaltung_add_zielgruppe/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Veranstaltung" ADD COLUMN     "zielgruppe" TEXT;
--- api/prisma/migrations/20240121150140_no_email_in_anmeldung/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `email` on the `Anmeldung` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Anmeldung" DROP COLUMN "email";
