74880d0741d07a707205a11a4a7258496ac6361d
danielswiatek <daniel.swiatek@codeanker.de>
2024-12-29 15:06:10 +0100

    refactor: simplify grid layout in FormPersonGeneral component

--- frontend/src/components/forms/person/FormPersonGeneral.vue
+++ frontend/src/components/forms/person/FormPersonGeneral.vue
@@ -148,7 +148,7 @@ const submit = () => {
 </script>
 
 <template>
-  <div class="grid grid-cols-1 lg:grid-cols-2">
+  <div class="grid">
     <ValidateForm @submit="submit">
       <Stammdaten v-model="stammdatenForm" />
       <hr class="my-5" />
