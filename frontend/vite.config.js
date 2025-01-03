d2f42f62a7051d8f5e2f1a5af49717657b81a58d
Barne Jürgensen <barne.juergensen@codeanker.de>
2025-01-03 22:52:04 +0100

    fix postcss export

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,20 +3,10 @@ import vue from '@vitejs/plugin-vue'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import path from 'path'
 import { pluginHttp2Proxy } from './pluginHttp2Proxy'
-import Components from 'unplugin-vue-components/vite'
-import RadixVueResolver from 'radix-vue/resolver'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    Components({
-      dts: true,
-      resolvers: [RadixVueResolver()],
-    }),
-    basicSsl(),
-    pluginHttp2Proxy(),
-  ],
+  plugins: [vue(), basicSsl(), pluginHttp2Proxy()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
