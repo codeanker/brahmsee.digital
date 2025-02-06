6b169025900e7612f59833f68ed61d251b38ea61
danielswiatek <daniel.swiatek@codeanker.de>
2025-02-06 19:20:44 +0100

    #210 set dayjs timezone to fix dates

--- packages/helpers/src/dayjs.ts
+++ packages/helpers/src/dayjs.ts
@@ -1,10 +1,14 @@
-import dayjs from 'dayjs'
+import dayjs, { type Dayjs } from 'dayjs'
 import customParseFormat from 'dayjs/plugin/customParseFormat.js'
-
 import 'dayjs/locale/de'
+import timezone from 'dayjs/plugin/timezone.js'
+import utc from 'dayjs/plugin/utc.js'
 
 dayjs.extend(customParseFormat)
-
+dayjs.extend(utc)
+dayjs.extend(timezone)
 dayjs.locale('de')
 
-export { dayjs }
+dayjs.tz.setDefault('Europe/Berlin')
+
+export { dayjs, type Dayjs }
