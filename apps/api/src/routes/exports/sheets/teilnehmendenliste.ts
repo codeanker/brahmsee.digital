135e993dd6d75a8164cdb94df84a51289c3db2d8
Axel Rindle <mail@axelrindle.de>
2025-02-06 22:01:39 +0100

    refactor: move export routes to routes directory

--- apps/api/src/routes/exports/sheets/teilnehmendenliste.ts
+++ apps/api/src/routes/exports/sheets/teilnehmendenliste.ts
@@ -1,72 +1,28 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { Role } from '@prisma/client'
 import dayjs from 'dayjs'
+import type { Context } from 'koa'
 import XLSX from 'xlsx'
-
-import { getEntityIdFromHeader } from '../../authentication.js'
-import prisma from '../../prisma.js'
-import { getGliederungRequireAdmin } from '../../util/getGliederungRequireAdmin.js'
+import { AnmeldungStatusMapping, GenderMapping } from '../../../client.js'
+import prisma from '../../../prisma.js'
 import { getSecurityWorksheet } from '../helpers/getSecurityWorksheet.js'
-import { AnmeldungStatusMapping } from '../../types/enums/mappings/AnmeldungStatus.js'
-import { GenderMapping } from '../../types/enums/mappings/Gender.js'
+import { sheetAuthorize } from './sheets.schema.js'
 
-export async function veranstaltungTeilnehmendenliste(ctx) {
-  const jwt = ctx.query.jwt
-  const accountId = getEntityIdFromHeader('Bearer ' + jwt)
-  const unterveranstaltungId = ctx.query.unterveranstaltungId
-  const veranstaltungId = ctx.query.veranstaltungId
-
-  if (accountId == null || (unterveranstaltungId == null && veranstaltungId == null)) {
-    ctx.res.statusCode = 401
-    ctx.res.end()
+export async function veranstaltungTeilnehmendenliste(ctx: Context) {
+  const authorization = await sheetAuthorize(ctx)
+  if (!authorization) {
     return
   }
 
-  const account = await prisma.account.findUnique({
-    where: {
-      id: parseInt(accountId),
-    },
-    select: {
-      role: true,
-      person: {
-        select: {
-          firstname: true,
-          lastname: true,
-        },
-      },
-    },
-  })
-
-  if (account == null) {
-    ctx.res.statusCode = 401
-    ctx.res.end()
-    return
-  }
-
-  let gliederung
-  if (account.role == Role.GLIEDERUNG_ADMIN) {
-    try {
-      gliederung = await getGliederungRequireAdmin(parseInt(accountId))
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    } catch (error) {
-      ctx.res.statusCode = 401
-      ctx.res.end()
-      return
-    }
-  }
+  const { query, account, gliederung } = authorization
 
   const anmeldungenList = await prisma.anmeldung.findMany({
     where: {
       OR: [
         {
-          unterveranstaltungId: parseInt(unterveranstaltungId || -1),
+          unterveranstaltungId: query.unterveranstaltungId,
         },
         {
           unterveranstaltung: {
-            veranstaltungId: parseInt(veranstaltungId || -1),
+            veranstaltungId: query.veranstaltungId,
           },
         },
       ],
@@ -182,13 +138,13 @@ export async function veranstaltungTeilnehmendenliste(ctx) {
   XLSX.utils.book_append_sheet(workbook, worksheet, `Teilnehmendenliste`)
 
   /** add Security Worksheet */
-  const { securityWorksheet, securityWorksheetName } = getSecurityWorksheet(XLSX, account, rows.length)
+  const { securityWorksheet, securityWorksheetName } = getSecurityWorksheet(account, rows.length)
   XLSX.utils.book_append_sheet(workbook, securityWorksheet, securityWorksheetName)
 
-  const buf = XLSX.write(workbook, { type: 'buffer', bookType: 'xlsx' })
-  /* prepare response headers */
-  ctx.res.statusCode = 200
   const filename = `${dayjs().format('YYYYMMDD-hhmm')}-Teilnehmenden.xlsx`
+  const buf = XLSX.write(workbook, { type: 'buffer', bookType: 'xlsx' }) as Buffer
+
+  ctx.res.statusCode = 201
   ctx.res.setHeader('Content-Disposition', `attachment; filename="${filename}"`)
   ctx.res.setHeader('Content-Type', 'application/vnd.ms-excel')
   ctx.res.end(buf)
--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,14 +1,17 @@
 import Router from 'koa-router'
-import addExports from '../exports/index.js'
 import addMiddlewares from '../middleware/index.js'
 
 import connect from './connect.js'
+import { veranstaltungTeilnehmendenliste } from './exports/sheets/teilnehmendenliste.js'
+import { veranstaltungVerpflegung } from './exports/sheets/verpflegung.js'
 
 const koaRouter = new Router()
 
 koaRouter.get('/connect/dlrg/callback', connect)
 
-addExports(koaRouter)
+koaRouter.get('/export/sheet/teilnehmendenliste', veranstaltungTeilnehmendenliste)
+koaRouter.get('/export/sheet/verpflegung', veranstaltungVerpflegung)
+
 addMiddlewares(koaRouter)
 
 export default koaRouter
