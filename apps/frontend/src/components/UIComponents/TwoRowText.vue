1a789099f1706aa8b2be72d95b72b5e88adca2b1
Axel Rindle <mail@axelrindle.de>
2025-04-07 21:30:51 +0200

    add component

---
+++ apps/frontend/src/components/UIComponents/TwoRowText.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+  subtitle: string
+}>()
+</script>
+
+<template>
+  <div>
+    <span>{{ title }}</span>
+    <br />
+    <span
+      class="text-sm"
+      v-html="subtitle"
+    />
+  </div>
+</template>
