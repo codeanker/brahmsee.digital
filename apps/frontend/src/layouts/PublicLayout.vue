4a3851956be31a5d152605285b0e5ede689a923f
Axel Rindle <mail@axelrindle.de>
2025-04-22 22:08:24 +0200

    update trailer video

--- apps/frontend/src/layouts/PublicLayout.vue
+++ apps/frontend/src/layouts/PublicLayout.vue
@@ -1,5 +1,23 @@
 <script lang="ts" setup>
 import Loading from '@/components/UIComponents/Loading.vue'
+import cn from '@/helpers/cn'
+import { PauseIcon, PlayIcon } from '@heroicons/vue/24/solid'
+import { useEventListener, useLocalStorage } from '@vueuse/core'
+import { useTemplateRef } from 'vue'
+
+const video = useTemplateRef('video')
+const paused = useLocalStorage('login-video-paused', false)
+
+useEventListener(video, 'pause', () => (paused.value = true))
+useEventListener(video, 'play', () => (paused.value = false))
+
+function toggle() {
+  if (video.value?.paused) {
+    video.value?.play()
+  } else {
+    video.value?.pause()
+  }
+}
 </script>
 
 <template>
@@ -12,17 +30,39 @@ import Loading from '@/components/UIComponents/Loading.vue'
         size="md"
         class="text-gray-300 dark:text-gray-700"
       />
+
       <video
+        ref="video"
         class="absolute object-cover w-full h-full bg-transparent"
-        autoplay
+        :autoplay="!paused"
         muted
         loop
       >
         <source
-          src="@/assets/images/trailer_temp.webm"
+          src="@/assets/images/brahmsee.digital.webm"
           type="video/webm"
         />
       </video>
+
+      <button
+        type="button"
+        :class="
+          cn(
+            'absolute inset-0 bg-black text-white',
+            paused ? 'bg-opacity-50' : 'transition-opacity opacity-0 bg-opacity-0 hover:bg-opacity-50 hover:opacity-100'
+          )
+        "
+        @click="toggle"
+      >
+        <PlayIcon
+          v-if="paused"
+          class="size-24 mx-auto"
+        />
+        <PauseIcon
+          v-else
+          class="size-24 mx-auto"
+        />
+      </button>
     </div>
 
     <!-- Grid Cell 2-->
