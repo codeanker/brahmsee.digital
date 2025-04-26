89ebc8b56794ca28a3a4d2add4593e43e573eac2
danielswiatek <daniel.swiatek@codeanker.de>
2025-04-26 19:00:36 +0200

    refactor: replace TwoRowText component with DataGridDoubleLineCell in UnterveranstaltungenTable

--- apps/frontend/src/components/UnterveranstaltungenTable.vue
+++ apps/frontend/src/components/UnterveranstaltungenTable.vue
@@ -9,7 +9,7 @@ import { apiClient } from '@/api'
 import BasicInput from '@/components/BasicInputs/BasicInput.vue'
 import { UnterveranstaltungTypeMapping, type RouterInput, type RouterOutput } from '@codeanker/api'
 import { type TGridColumn } from '@codeanker/datagrid'
-import TwoRowText from './UIComponents/TwoRowText.vue'
+import DataGridDoubleLineCell from './DataGridDoubleLineCell.vue'
 
 const props = defineProps<{
   veranstaltungId?: number
@@ -33,11 +33,13 @@ const columns = computed<TGridColumn<TUnterveranstaltungData, TUnterveranstaltun
     {
       field: 'veranstaltung.name',
       title: 'Veranstaltung',
-      cell: TwoRowText,
-      cellProps: (_, { content }) => ({
-        title: content.veranstaltung.name,
-        subtitle: content.beschreibung.split(' ').slice(0, 5).join(' '),
-      }),
+      cell: DataGridDoubleLineCell,
+      cellProps: (formattedValue, row) => {
+        return {
+          title: row.content.veranstaltung.name,
+          message: row.content.beschreibung.split(' ').slice(0, 5).join(' '),
+        }
+      },
     },
     {
       field: 'gliederung.name',
