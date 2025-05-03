a97d8c975b6c3de1ed0d4e558f272a861fed3732
Axel Rindle <mail@axelrindle.de>
2025-05-03 13:31:47 +0200

    update person table

--- apps/api/src/services/person/personList.ts
+++ apps/api/src/services/person/personList.ts
@@ -2,37 +2,57 @@ import { Prisma, Role } from '@prisma/client'
 import z from 'zod'
 
 import prisma from '../../prisma.js'
-import { defineProtectedQueryProcedure } from '../../types/defineProcedure.js'
-import { defineQuery, getOrderBy } from '../../types/defineQuery.js'
 import type { AuthenticatedContext } from '../../trpc.js'
-
-const inputSchema = defineQuery({
-  filter: z.strictObject({
-    name: z.string().optional(),
-    gliederungName: z.string().optional(),
-  }),
-  orderBy: z.array(
-    z.tuple([
-      z.union([z.literal('firstname'), z.literal('birthday'), z.literal('gliederung.name')]),
-      z.union([z.literal('asc'), z.literal('desc')]),
-    ])
-  ),
-})
-
-type TInput = z.infer<typeof inputSchema>
+import { defineProtectedQueryProcedure } from '../../types/defineProcedure.js'
+import { calculatePagination, defineQueryResponse, defineTableInput } from '../../types/defineTableProcedure.js'
+import { getGliederungRequireAdmin } from '../../util/getGliederungRequireAdmin.js'
 
 export const personListProcedure = defineProtectedQueryProcedure({
   key: 'list',
   roleIds: [Role.ADMIN, Role.USER],
-  inputSchema,
-  handler: ({ ctx, input }) => {
-    const { skip, take } = input.pagination
+  inputSchema: defineTableInput({
+    filter: {
+      name: z.string(),
+      gliederung_name: z.string(),
+      // gliederungId: z.number().int(),
+      // veranstaltungId: z.number().int(),
+    },
+  }),
+  handler: async ({ ctx, input: { filter, pagination } }) => {
+    const protection = await getPersonProtectionFilter(ctx)
+    const where: Prisma.PersonWhereInput = {
+      OR: filter?.name
+        ? [
+            {
+              firstname: {
+                contains: filter.name,
+                mode: 'insensitive',
+              },
+            },
+            {
+              lastname: {
+                contains: filter.name,
+                mode: 'insensitive',
+              },
+            },
+          ]
+        : undefined,
+      gliederung: {
+        name: filter?.gliederung_name,
+      },
+      ...protection,
+    }
 
-    return prisma.person.findMany({
-      skip,
-      take,
-      where: getWhere(input.filter, ctx),
-      orderBy: getOrderBy(input.orderBy),
+    const total = await prisma.person.count({ where })
+    const { pageIndex, pageSize, pages } = calculatePagination(total, pagination)
+
+    const persons = await prisma.person.findMany({
+      take: pageSize,
+      skip: pageSize * pageIndex,
+      orderBy: {
+        lastname: 'asc',
+      },
+      where,
       select: {
         id: true,
         firstname: true,
@@ -41,20 +61,20 @@ export const personListProcedure = defineProtectedQueryProcedure({
         photoId: true,
         gliederung: {
           select: {
-            id: true,
             name: true,
           },
         },
         account: {
           select: {
             id: true,
-            activatedAt: true,
-            role: true,
-            status: true,
-            GliederungToAccount: {
+          },
+        },
+        anmeldungen: {
+          select: {
+            _count: true,
+            unterveranstaltung: {
               select: {
-                role: true,
-                gliederung: {
+                veranstaltung: {
                   select: {
                     name: true,
                   },
@@ -65,24 +85,14 @@ export const personListProcedure = defineProtectedQueryProcedure({
         },
       },
     })
+
+    return defineQueryResponse({ data: persons, total, pagination: { pageIndex, pageSize, pages } })
   },
 })
 
-export const personCountProcedure = defineProtectedQueryProcedure({
-  key: 'count',
-  roleIds: [Role.ADMIN, Role.USER],
-  inputSchema: inputSchema.pick({ filter: true }),
-  async handler(options) {
-    const total = await prisma.person.count({
-      where: getWhere(options.input.filter, options.ctx),
-    })
-    return total
-  },
-})
-
-export function getPersonProtectionFilter({
+export async function getPersonProtectionFilter({
   account,
-}: AuthenticatedContext): Prisma.PersonWhereInput | Prisma.PersonWhereUniqueInput {
+}: AuthenticatedContext): Promise<Prisma.PersonWhereInput | Prisma.PersonWhereUniqueInput> {
   const where: Prisma.PersonWhereInput = {}
 
   if (account.role === Role.USER) {
@@ -91,27 +101,10 @@ export function getPersonProtectionFilter({
         accountId: account.id,
       },
     }
+  } else if (account.role === Role.GLIEDERUNG_ADMIN) {
+    const gliederung = await getGliederungRequireAdmin(account.id)
+    where.gliederungId = gliederung.id
   }
 
   return where
-}
-
-function getWhere(filter: TInput['filter'], ctx: AuthenticatedContext): Prisma.PersonWhereInput {
-  const where: Prisma.PersonWhereInput = {}
-
-  if (filter.name != null && filter.name != '') {
-    where.OR = [{ firstname: { contains: filter.name } }, { lastname: { contains: filter.name } }]
-  }
-  if (filter.gliederungName != null && filter.gliederungName != '') {
-    where.gliederung = {
-      name: {
-        contains: filter.gliederungName,
-      },
-    }
-  }
-
-  return {
-    ...where,
-    ...getPersonProtectionFilter(ctx),
-  }
 }
