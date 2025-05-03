a8abd4c113d60351703e3e69a205f2a3de046dd4
Axel Rindle <mail@axelrindle.de>
2025-05-03 13:31:59 +0200

    fix dashboard

--- apps/frontend/src/views/Dashboard/Verwaltung.vue
+++ apps/frontend/src/views/Dashboard/Verwaltung.vue
@@ -3,15 +3,16 @@ import { useAsyncState } from '@vueuse/core'
 
 import { apiClient } from '@/api'
 import VeranstaltungCard from '@/components/UIComponents/VeranstaltungCard.vue'
+import initialData from '@/components/Table/initialData'
 
 const { state: veranstaltungen } = useAsyncState(async () => {
-  return apiClient.veranstaltung.verwaltungList.query({ filter: {}, orderBy: [], pagination: { take: 100, skip: 0 } })
-}, [])
+  return apiClient.veranstaltung.verwaltungList.query({})
+}, initialData)
 </script>
 
 <template>
   <VeranstaltungCard
-    v-for="veranstaltung in veranstaltungen"
+    v-for="veranstaltung in veranstaltungen.data"
     :key="veranstaltung.id"
     :veranstaltung="veranstaltung"
   />
