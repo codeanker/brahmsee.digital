9e202030f4eb05893d1799ab9de76460252adaa6
Axel Rindle <mail@axelrindle.de>
2025-05-10 18:26:35 +0200

    fix: add default column value

--- apps/api/prisma/migrations/20250510143106_veranstaltung_public_read_token/migration.sql
+++ apps/api/prisma/migrations/20250510143106_veranstaltung_public_read_token/migration.sql
@@ -4,8 +4,10 @@
   - A unique constraint covering the columns `[publicReadToken]` on the table `Veranstaltung` will be added. If there are existing duplicate values, this will fail.
 
 */
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 -- AlterTable
-ALTER TABLE "Veranstaltung" ADD COLUMN     "publicReadToken" TEXT;
+ALTER TABLE "Veranstaltung" ADD COLUMN     "publicReadToken" TEXT DEFAULT uuid_generate_v4();
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Veranstaltung_publicReadToken_key" ON "Veranstaltung"("publicReadToken");
