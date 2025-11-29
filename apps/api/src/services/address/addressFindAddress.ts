26fed4c5e4f13979e0969a9d2b97184ac0ed6d74
Axel Rindle <mail@axelrindle.de>
2025-11-29 13:47:29 +0100

    remove axios

--- apps/api/src/services/address/addressFindAddress.ts
+++ apps/api/src/services/address/addressFindAddress.ts
@@ -1,8 +1,28 @@
-import axios from 'axios'
 import z from 'zod'
 
 import config from '../../config.js'
 import { definePublicQueryProcedure } from '../../types/defineProcedure.js'
+
+const ZTomTomSearchResponse = z.object({
+  summary: z.object({
+    numResults: z.number(),
+  }),
+  results: z.array(
+    z.object({
+      address: z.object({
+        streetName: z.string(),
+        streetNumber: z.string(),
+        postalCode: z.string(),
+        municipality: z.string(),
+        countryCode: z.string(),
+      }),
+      position: z.object({
+        lat: z.number(),
+        lon: z.number(),
+      }),
+    })
+  ),
+})
 
 export const addressFindActionProcedure = definePublicQueryProcedure({
   key: 'findAddress',
@@ -14,7 +34,6 @@ export const addressFindActionProcedure = definePublicQueryProcedure({
     streetNumber: z.string().optional(),
     country: z.string().optional(),
   }),
-
   async handler(options) {
     let searchText = ''
     if (options.input.query != null) {
@@ -42,50 +61,41 @@ export const addressFindActionProcedure = definePublicQueryProcedure({
     }
     const country = options?.input?.country != null ? options.input.country.toUpperCase() : 'DE'
 
-    try {
-      const query = await axios.get(
-        `https://api.tomtom.com/search/2/search/${encodeURIComponent(
-          searchText
-        )}.json?typeahead=true&limit=5&countrySet=${country}&language=${language}&idxSet=PAD&minFuzzyLevel=1&maxFuzzyLevel=2&view=Unified&key=${token}`
-      )
+    const url = new URL(`https://api.tomtom.com/search/2/search/${encodeURIComponent(searchText)}.json`)
+    url.searchParams.append('typeahead', 'true')
+    url.searchParams.append('limit', '5')
+    url.searchParams.append('countrySet', country)
+    url.searchParams.append('language', language)
+    url.searchParams.append('idxSet', 'PAD')
+    url.searchParams.append('minFuzzyLevel', '1')
+    url.searchParams.append('maxFuzzyLevel', '2')
+    url.searchParams.append('view', 'Unified')
+    url.searchParams.append('key', token)
 
-      const ZTomTomSearchResponse = z.object({
-        summary: z.object({
-          numResults: z.number(),
-        }),
-        results: z.array(
-          z.object({
-            address: z.object({
-              streetName: z.string(),
-              streetNumber: z.string(),
-              postalCode: z.string(),
-              municipality: z.string(),
-              countryCode: z.string(),
-            }),
-            position: z.object({
-              lat: z.number(),
-              lon: z.number(),
-            }),
-          })
-        ),
-      })
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    })
 
-      const parsedSearchResponse = ZTomTomSearchResponse.parse(query.data)
-
-      if (parsedSearchResponse.summary.numResults < 1) return []
-      return parsedSearchResponse.results.map((result) => {
-        return {
-          street: result.address.streetName,
-          streetNumber: result.address.streetNumber,
-          zip: result.address.postalCode,
-          city: result.address.municipality,
-          country: result.address.countryCode,
-          position: result.position,
-        }
-      })
-    } catch (e) {
-      console.error(e)
+    if (response.status !== 200) {
       return []
     }
+
+    const body = await response.json()
+    const parsedSearchResponse = ZTomTomSearchResponse.parse(body)
+
+    if (parsedSearchResponse.summary.numResults < 1) return []
+    return parsedSearchResponse.results.map((result) => {
+      return {
+        street: result.address.streetName,
+        streetNumber: result.address.streetNumber,
+        zip: result.address.postalCode,
+        city: result.address.municipality,
+        country: result.address.countryCode,
+        position: result.position,
+      }
+    })
   },
 })
