0a8ed0b58ec941ba715d7a67f6aeadedeff75c30
Axel Rindle <mail@axelrindle.de>
2026-01-10 22:27:36 +0100

    make account creation protected

--- apps/api/src/services/account/accountGliederungAdminCreate.ts
+++ apps/api/src/services/account/accountGliederungAdminCreate.ts
@@ -1,18 +1,16 @@
-import { Gender } from '@prisma/client'
 import { TRPCError } from '@trpc/server'
-import jwt from 'jsonwebtoken'
-import z from 'zod'
 
-import config from '../../config.js'
 import prisma from '../../prisma.js'
-import { ZOauthRegisterJwtPayloadSchema } from '../../routes/connect.js'
-import { definePublicMutateProcedure } from '../../types/defineProcedure.js'
+import { defineProtectedMutateProcedure } from '../../types/defineProcedure.js'
 
 import { sendMailConfirmEmailRequest } from './helpers/sendMailConfirmEmailRequest.js'
 import { getAccountCreateData } from './schema/account.schema.js'
+import { z } from 'zod'
+import { Gender } from '@prisma/client'
 
-const ZAccountGliederungAdminCreateInput = z.strictObject({
-  data: z.strictObject({
+export const accountGliederungAdminCreateProcedure = defineProtectedMutateProcedure({
+  key: 'gliederungAdminCreate',
+  inputSchema: z.strictObject({
     firstname: z.string(),
     lastname: z.string(),
     gender: z.nativeEnum(Gender),
@@ -20,48 +18,31 @@ const ZAccountGliederungAdminCreateInput = z.strictObject({
     email: z.string().email().optional(), // email is required, because oauth login does not have an email
     password: z.string().optional(), // optional, because oauth login does not have a password
     gliederungId: z.number().int(),
-    jwtOAuthToken: z.string().optional(), // optional, becaus normal registration does not have a jwtOAuthToken
   }),
-})
-
-export const accountGliederungAdminCreateProcedure = definePublicMutateProcedure({
-  key: 'gliederungAdminCreate',
-  inputSchema: ZAccountGliederungAdminCreateInput,
-  async handler(options) {
-    let dlrgOauthId: undefined | string = undefined
-    // check if jwtOAuthToken set and if so, check if it is valid
-
-    if (options.input.data.jwtOAuthToken) {
-      const jwtOAuthTokenPayload = ZOauthRegisterJwtPayloadSchema.parse(
-        jwt.verify(options.input.data.jwtOAuthToken, `${config.authentication.secret}-oauth`)
-      )
-      options.input.data.email = jwtOAuthTokenPayload.email
-      dlrgOauthId = jwtOAuthTokenPayload.sub
-    }
-
-    if (!options.input.data.email) {
+  roleIds: ['ADMIN'],
+  handler: async ({ input }) => {
+    if (!input.email) {
       throw new TRPCError({
         code: 'BAD_REQUEST',
         message: 'Email muss angegeben werden',
       })
     }
+
     const accountData = await getAccountCreateData({
-      email: options.input.data.email,
-      firstname: options.input.data.firstname,
-      lastname: options.input.data.lastname,
-      password: options.input.data.password,
-      birthday: options.input.data.birthday,
-      gender: options.input.data.gender,
+      email: input.email,
+      firstname: input.firstname,
+      lastname: input.lastname,
+      password: input.password,
+      birthday: input.birthday,
+      gender: input.gender,
       roleId: 'GLIEDERUNG_ADMIN',
       isActiv: false,
-      gliederungId: options.input.data.gliederungId,
-      adminInGliederungId: options.input.data.gliederungId,
+      gliederungId: input.gliederungId,
+      adminInGliederungId: input.gliederungId,
     })
+
     const res = await prisma.account.create({
-      data: {
-        ...accountData,
-        dlrgOauthId,
-      },
+      data: accountData,
       select: {
         id: true,
       },
--- apps/api/src/services/account/accountTeilnehmerCreate.ts
+++ apps/api/src/services/account/accountTeilnehmerCreate.ts
@@ -1,18 +1,18 @@
 import { Gender } from '@prisma/client'
 import { TRPCError } from '@trpc/server'
-import jwt from 'jsonwebtoken'
 import z from 'zod'
 
-import config from '../../config.js'
 import prisma from '../../prisma.js'
-import { ZOauthRegisterJwtPayloadSchema } from '../../routes/connect.js'
-import { definePublicMutateProcedure } from '../../types/defineProcedure.js'
+import { defineProtectedMutateProcedure } from '../../types/defineProcedure.js'
 
 import { sendMailConfirmEmailRequest } from './helpers/sendMailConfirmEmailRequest.js'
 import { getAccountCreateData } from './schema/account.schema.js'
+// import { ZOauthRegisterJwtPayloadSchema } from '../../routes/oidc/connect.js'
 
-const ZAccountTeilnehmerCreateInput = z.strictObject({
-  data: z.strictObject({
+export const accountTeilnehmerCreateProcedure = defineProtectedMutateProcedure({
+  key: 'teilnehmerCreate',
+  roleIds: ['ADMIN'],
+  inputSchema: z.strictObject({
     firstname: z.string(),
     lastname: z.string(),
     gender: z.nativeEnum(Gender),
@@ -20,47 +20,28 @@ const ZAccountTeilnehmerCreateInput = z.strictObject({
     email: z.string().email().optional(), // email is required, because oauth login does not have an email
     password: z.string().optional(), // optional, because oauth login does not have a password
     gliederungId: z.number().int(),
-    jwtOAuthToken: z.string().optional(), // optional, becaus normal registration does not have a jwtOAuthToken
   }),
-})
-
-export const accountTeilnehmerCreateProcedure = definePublicMutateProcedure({
-  key: 'teilnehmerCreate',
-  inputSchema: ZAccountTeilnehmerCreateInput,
-  async handler(options) {
-    let dlrgOauthId: undefined | string = undefined
-    // check if jwtOAuthToken set and if so, check if it is valid
-
-    if (options.input.data.jwtOAuthToken) {
-      const jwtOAuthTokenPayload = ZOauthRegisterJwtPayloadSchema.parse(
-        jwt.verify(options.input.data.jwtOAuthToken, `${config.authentication.secret}-oauth`)
-      )
-      options.input.data.email = jwtOAuthTokenPayload.email
-      dlrgOauthId = jwtOAuthTokenPayload.sub
-    }
-
-    if (!options.input.data.email) {
+  handler: async ({ input }) => {
+    if (!input.email) {
       throw new TRPCError({
         code: 'BAD_REQUEST',
         message: 'Email muss angegeben werden',
       })
     }
     const accountData = await getAccountCreateData({
-      email: options.input.data.email,
-      firstname: options.input.data.firstname,
-      lastname: options.input.data.lastname,
-      password: options.input.data.password,
-      birthday: options.input.data.birthday,
-      gender: options.input.data.gender,
+      email: input.email,
+      firstname: input.firstname,
+      lastname: input.lastname,
+      password: input.password,
+      birthday: input.birthday,
+      gender: input.gender,
       roleId: 'USER',
       isActiv: false,
-      gliederungId: options.input.data.gliederungId,
+      gliederungId: input.gliederungId,
     })
+
     const res = await prisma.account.create({
-      data: {
-        ...accountData,
-        dlrgOauthId,
-      },
+      data: accountData,
       select: {
         id: true,
       },
