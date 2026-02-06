8531fd06415441208b3d5de9f421acade5bb0c53
Axel Rindle <axel@rindle.dev>
2026-02-06 12:25:44 +0100

    fix: route protection

--- apps/api/src/routes/exports/index.ts
+++ apps/api/src/routes/exports/index.ts
@@ -1,12 +1,13 @@
 import { makeApp } from '../../util/make-app.js'
+import { authorize } from './middleware/authorize.js'
 import { veranstaltungPhotoArchive } from './photos.archive.js'
 import { veranstaltungTeilnehmendenliste } from './teilnehmendenliste.sheet.js'
 import { veranstaltungVerpflegung } from './verpflegung.sheet.js'
 
 const exportRouter = makeApp()
-
-exportRouter.get('/sheet/teilnehmendenliste', veranstaltungTeilnehmendenliste)
-exportRouter.get('/sheet/verpflegung', veranstaltungVerpflegung)
-exportRouter.get('/archive/photos', veranstaltungPhotoArchive)
+  .use(authorize)
+  .get('/sheet/teilnehmendenliste', veranstaltungTeilnehmendenliste)
+  .get('/sheet/verpflegung', veranstaltungVerpflegung)
+  .get('/archive/photos', veranstaltungPhotoArchive)
 
 export { exportRouter }
--- apps/api/src/routes/exports/sheets.schema.ts
+++ apps/api/src/routes/exports/sheets.schema.ts
@@ -1,10 +1,4 @@
-import { Role, type Gliederung } from '@prisma/client'
-import type { Context } from 'hono'
 import { z } from 'zod'
-import { getEntityIdFromHeader } from '../../authentication.js'
-import prisma from '../../prisma.js'
-import { getGliederungRequireAdmin } from '../../util/getGliederungRequireAdmin.js'
-import { zodSafe } from '../../util/zod.js'
 
 export const sheetQuerySchema = z
   .object({
@@ -17,50 +11,3 @@ export const sheetQuerySchema = z
   })
 
 export type SheetQuery = z.infer<typeof sheetQuerySchema>
-
-export async function sheetAuthorize(ctx: Context) {
-  const [success, query] = await zodSafe(sheetQuerySchema, ctx.req.query())
-  if (!success) {
-    ctx.status(400)
-    return false
-  }
-
-  const accountId = getEntityIdFromHeader(`Bearer ${query.jwt}`)
-  if (typeof accountId !== 'string') {
-    ctx.status(401)
-    return false
-  }
-
-  const account = await prisma.account.findUnique({
-    where: {
-      id: accountId,
-    },
-    select: {
-      role: true,
-      person: {
-        select: {
-          firstname: true,
-          lastname: true,
-        },
-      },
-    },
-  })
-
-  if (account == null) {
-    ctx.status(401)
-    return false
-  }
-
-  let gliederung: Gliederung | undefined = undefined
-  if (account.role == Role.GLIEDERUNG_ADMIN) {
-    try {
-      gliederung = await getGliederungRequireAdmin(accountId)
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    } catch (error) {
-      ctx.status(401)
-      return false
-    }
-  }
-
-  return { query, account, gliederung }
-}
