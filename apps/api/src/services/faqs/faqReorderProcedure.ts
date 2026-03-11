b952c1e8a8a6a26f983c55130d61c276e301ab1d
Momme Jürgensen <momme@juergensen.me>
2026-03-11 20:11:07 +0100

    fix: add permissions checks

--- apps/api/src/services/faqs/faqReorderProcedure.ts
+++ apps/api/src/services/faqs/faqReorderProcedure.ts
@@ -1,7 +1,10 @@
 import z from 'zod'
 
 import prisma from '../../prisma.js'
+import { TRPCError } from '@trpc/server'
+import { Role } from '@prisma/client'
 import { defineProtectedMutateProcedure } from '../../types/defineProcedure.js'
+import { getGliederungRequireAdmin } from '../../util/getGliederungRequireAdmin.js'
 
 export const faqReorderProcedure = defineProtectedMutateProcedure({
   key: 'reorder',
@@ -21,7 +24,59 @@ export const faqReorderProcedure = defineProtectedMutateProcedure({
       })
     ),
   }),
-  handler: async ({ input: { faqOrder, categoryOrder } }) => {
+  handler: async ({ ctx, input: { faqOrder, categoryOrder } }) => {
+    /// Check permissions for FAQs and categories
+    if (ctx.account.role !== Role.ADMIN) {
+      const gliederung = await getGliederungRequireAdmin(ctx.accountId)
+      const unterveranstaltungIds = (
+        await prisma.unterveranstaltung.findMany({
+          where: { gliederungId: gliederung.id },
+          select: { id: true },
+        })
+      ).map((u) => u.id)
+
+      const faqs = await prisma.faq.findMany({
+        where: { id: { in: faqOrder.map((f) => f.id) } },
+        select: {
+          id: true,
+          unterveranstaltung: {
+            select: {
+              id: true,
+            },
+          },
+        },
+      })
+      for (const faq of faqs) {
+        const unterveranstaltungId = faq.unterveranstaltung[0]?.id
+        if (!unterveranstaltungId || !unterveranstaltungIds.includes(unterveranstaltungId)) {
+          throw new TRPCError({ code: 'FORBIDDEN', message: `FAQ '${faq.id}' do not belong to accounts gliederung` })
+        }
+      }
+
+      const categoryIds = [...new Set([...categoryOrder.map((c) => c.id), ...faqOrder.map((f) => f.categoryId)])]
+      const categories = await prisma.faqCategory.findMany({
+        where: { id: { in: categoryIds } },
+        select: {
+          id: true,
+          unterveranstaltung: {
+            select: {
+              id: true,
+            },
+          },
+        },
+      })
+      for (const category of categories) {
+        const unterveranstaltungId = category.unterveranstaltung.id
+        if (!unterveranstaltungId || !unterveranstaltungIds.includes(unterveranstaltungId)) {
+          throw new TRPCError({
+            code: 'FORBIDDEN',
+            message: `FAQ Category '${category.id}' do not belong to accounts gliederung`,
+          })
+        }
+      }
+    }
+
+    /// Perform updates in a transaction
     await prisma.$transaction([
       ...faqOrder.map(({ id, sortOrder, categoryId }) =>
         prisma.faq.update({
